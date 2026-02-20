a7182de32c0454410f58afe69b1520db5eec9799
Emmett Cohen <emcohen101@gmail.com>
2026-02-20 10:49:53 -0500

    Emmett Mad Lib

--- emmett.py
+++ emmett.py
@@ -1,2 +1,9 @@
 def emmett_mad_lib():
-    print("Welcome!")
+    date = input("Enter a date (MM/DD/YYYY): ")
+    name = input("Enter a name: ")
+    adj = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    sig = input("Enter another name: ")
+
+    print("\n\n- YOUR STORY -")
+    print(f"Date: {date}\n\nPlease excuse {name}, who is far too {adj} to attend {noun} class.\n\n Signed,\n{sig}\n")
