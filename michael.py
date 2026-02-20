c3aeac0441070bf075d151cc5a9064a24a03a816
Michael-Isaacson <misaacaon@icloud.com>
2026-02-20 10:50:10 -0500

    Michael Mad Lib Pasted

--- michael.py
+++ michael.py
@@ -1,2 +1,14 @@
 def michael_mad_lib():
-    print("Welcome!")
+    date = input("Enter a date (MM/DD/YYYY): ")
+    name = input("Enter a name: ")
+    adj = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    sig = input("Enter another name: ")
+
+    print(f"\n\n- YOUR STORY -")
+    print(f"Date: {date}")
+    print(f"\nPlease excuse {name} who is far too {adj} to attend {noun} class.")
+    print("Signed:")
+    print(sig)
+
+michael_mad_lib()
